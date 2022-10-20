7459aa1eeef7a8ec6651dfc07493904008efbef2
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-10-20 19:58:29 -0400

    created template for ChefRegistration.java

--- app/src/main/java/ca/uottawa/engineering/mealer/ChefRegistration.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/ChefRegistration.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import ca.uottawa.engineering.mealer.classes.Chef;
+
 public class ChefRegistration extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final String role = "chef";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_registration);
     }
+
+    private void register() {
+        mAuth = FirebaseAuth.getInstance();
+
+        // TODO: get information about chef
+
+        Chef chef = new Chef();
+        db.collection("users").document(mAuth.getUid()).set(chef);
+    }
+
 }
