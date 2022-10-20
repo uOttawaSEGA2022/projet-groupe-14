39153d25eee486c2e513a4427947562e9367e4ae
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-10-20 17:49:32 -0400

    Homepage displays role os role of user

--- app/src/main/java/ca/uottawa/engineering/mealer/Homepage.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/Homepage.java
@@ -2,15 +2,24 @@ package ca.uottawa.engineering.mealer;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Homepage extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final String TAG = "Test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +30,30 @@ public class Homepage extends AppCompatActivity {
         String userId = mAuth.getUid();
 
         TextView hText = (TextView) findViewById(R.id.homepageText);
-        hText.setText(String.format("You are signed in as %s", userId));
+
+        DocumentReference docRef = db.collection("users").document(userId);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        String role = document.get("role").toString();
+                        hText.setText(String.format("You are signed in as %s", role));
+
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+
+
     }
 
 }
