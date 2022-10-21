04d5db57105bf9372c353b1f58a6e9eac8619e68
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-10-21 18:55:18 -0400

    added basic validation for inputs in client and chef reg

--- app/src/main/java/ca/uottawa/engineering/mealer/ChefRegistration.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/ChefRegistration.java
@@ -33,55 +33,49 @@ public class ChefRegistration extends AppCompatActivity {
         setContentView(R.layout.activity_chef_registration);
     }
 
+    // Register button press
     public void register(View view) throws InterruptedException {
-        EditText nickT = (EditText)findViewById(R.id.NicknameEdit);
-        EditText nameT = (EditText)findViewById(R.id.NameEdit);
-        EditText emailT = (EditText)findViewById(R.id.EmailEdit);
-        EditText passT = (EditText)findViewById(R.id.chefPasswordEdit);
-        EditText addrT = (EditText)findViewById(R.id.AdressEdit);
-        EditText chequeT = (EditText)findViewById(R.id.informationchequeEdit);
-        EditText descT = (EditText)findViewById(R.id.DescriptionEdit);
 
-        try {
-            String nickname = nickT.getText().toString();
-            String name = nameT.getText().toString();
-            String email = emailT.getText().toString();
-            String password = passT.getText().toString();
-            String address = addrT.getText().toString();
-            String cheque = chequeT.getText().toString();
-            String description = descT.getText().toString();
+        errorMessage("");
+        String[] inputs = getInput();
+        boolean valid = validateInput(inputs);
 
-            Log.d(TAG, email);
-            Log.d(TAG, password);
+        if (!valid) {
+            return;
+        }
 
-            mAuth.createUserWithEmailAndPassword(email, password) // Create new user
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                mAuth = FirebaseAuth.getInstance();
-                                String userID = mAuth.getUid();
-                                Log.d(TAG, userID);
+        String nickname = inputs[0];
+        String name = inputs[1];
+        String email = inputs[2];
+        String password = inputs[3];
+        String address = inputs[4];
+        String cheque = inputs[5];
+        String description = inputs[6];
 
-                                Chef chef = new Chef(nickname, name, email, address, cheque, description);
-                                db.collection("users").document(userID).set(chef); // Add to database
-                                Log.d(TAG, "Chef created and added.");
+        Log.d(TAG, email);
+        Log.d(TAG, password);
 
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.d(TAG, "Chef not created.");
-                                // TODO: add message to user
-                                errormsgEmail(null);
-                            }
+        mAuth.createUserWithEmailAndPassword(email, password) // Create new user
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            mAuth = FirebaseAuth.getInstance();
+                            String userID = mAuth.getUid();
+                            Log.d(TAG, userID);
+
+                            Chef chef = new Chef(nickname, name, email, address, cheque, description);
+                            db.collection("users").document(userID).set(chef); // Add to database
+                            Log.d(TAG, "Chef created and added.");
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d(TAG, "Chef not created.");
+                            // TODO: add message to user
                         }
-                    });
-
-        }
-        catch (Exception e){
-            errormsgEmpty(null);
-        }
-
+                    }
+                });
 
         TimeUnit.SECONDS.sleep(1); // Sometimes it goes too fast and the database doesn't update fast enough
         if (mAuth.getUid() != null) {
@@ -89,15 +83,55 @@ public class ChefRegistration extends AppCompatActivity {
             startActivity(switchActivityIntent);
         }
     }
-    public void errormsgEmpty(View view){
-        TextView errormsg =(TextView)findViewById(R.id.errormsgchef);
-        errormsg.setText("error:one or more fields are empty");
-    }
-    public void errormsgEmail(View view){
-        TextView errormsg =(TextView)findViewById(R.id.errormsgchef);
-        errormsg.setText("error:incorrect email");
 
+    public void errorMessage(String message) {
+        TextView errormsg = (TextView) findViewById(R.id.errormsgchef);
+        errormsg.setText(message);
     }
 
+    public boolean validateInput(String[] inputs) {
+
+        for (String input: inputs) {
+            input = input.trim();
+            if (input.isEmpty()) {
+                errorMessage("Please make sure fields aren't empty.");
+                return false;
+            }
+        }
+
+        if (!inputs[2].contains("@")) {
+            errorMessage("Please enter a valid email address.");
+            return false;
+        }
+        if (inputs[3].length() < 6) {
+            errorMessage("Please have a password that is at least 6 characters long.");
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public String[] getInput() {
+        String[] inputs = new String[7];
+
+        EditText nickT = (EditText) findViewById(R.id.NicknameEdit);
+        EditText nameT = (EditText) findViewById(R.id.NameEdit);
+        EditText emailT = (EditText) findViewById(R.id.EmailEdit);
+        EditText passT = (EditText) findViewById(R.id.chefPasswordEdit);
+        EditText addrT = (EditText) findViewById(R.id.AdressEdit);
+        EditText chequeT = (EditText) findViewById(R.id.informationchequeEdit);
+        EditText descT = (EditText) findViewById(R.id.DescriptionEdit);
+
+        inputs[0] = nickT.getText().toString();
+        inputs[1] = nameT.getText().toString();
+        inputs[2] = emailT.getText().toString();
+        inputs[3] = passT.getText().toString();
+        inputs[4] = addrT.getText().toString();
+        inputs[5] = chequeT.getText().toString();
+        inputs[6] = descT.getText().toString();
+
+        return inputs;
+    }
 
 }
