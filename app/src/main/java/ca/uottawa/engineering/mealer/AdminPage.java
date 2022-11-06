58bd994ff1bdc17ba39656b1daa5b4211a9c3e9b
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-11-06 13:12:15 -0500

    Complaints now fetched on AdminPage, modified classes
    Classes were modified to support complaint fetching. AdminPage will
    fetch complaints and put them in a list. Complaint class now contains a
    reference to chef on firebase as well as an instance of chef.

--- app/src/main/java/ca/uottawa/engineering/mealer/AdminPage.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/AdminPage.java
@@ -2,21 +2,71 @@ package ca.uottawa.engineering.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import android.os.Bundle;
 import android.widget.ListView;
 
-import ca.uottawa.engineering.mealer.classes.Admin;
+import org.checkerframework.checker.units.qual.C;
 
-public class AdminPage extends AppCompatActivity{
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+
+import ca.uottawa.engineering.mealer.classes.Chef;
+import ca.uottawa.engineering.mealer.classes.Complaint;
+
+public class AdminPage extends AppCompatActivity {
     ListView listView;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private ArrayList<Complaint> complaints = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Admin admin = new Admin();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_page);
+        retrieveComplaints();
         listView = (ListView) findViewById(R.id.list);
         //initialiser le custombaseadaptor avec la list de complaint comme argument
         //listview.setAdapter(custombaseadaptor) pour configurer la list a afficher tous les complains dans l ordre
+    }
+
+    public void retrieveComplaints() {
+        final String TAG = "COMPLAINT";
+
+        db.collection("complaints")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Map complaintData = document.getData();
+                                Timestamp timestamp = (Timestamp) complaintData.get("date_created");
+
+                                DocumentReference documentReference = (DocumentReference) complaintData.get("chef");
+                                Complaint complaint = new Complaint(timestamp.toDate(), documentReference);
+                                complaints.add(complaint);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/ca/uottawa/engineering/mealer/classes/Complaint.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/classes/Complaint.java
@@ -1,23 +1,36 @@
 package ca.uottawa.engineering.mealer.classes;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+
 import java.util.Date;
 
 public class Complaint {
 
     private final Date PERMANENT_SUSPENSION = new Date(1);
 
-    private final Chef chef;
+    private Date date_created;
+    private DocumentReference chefRef;
+    private Chef chef;
 
-    public Complaint(Chef chef) {
+    public Complaint(Date date_created, DocumentReference chefRef) {
+        this.date_created = date_created;
+        this.chefRef = chefRef;
+
+        chefRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Chef chef = documentSnapshot.toObject(Chef.class);
+                setChef(chef);
+            }
+        });    }
+
+    public Chef getChef() {
+        return chef;
+    }
+
+    public void setChef(Chef chef) {
         this.chef = chef;
     }
-
-    public void suspend(Date date) {
-        chef.setSuspension(date);
-    }
-
-    public void perm_suspend() {
-        chef.setSuspension(PERMANENT_SUSPENSION);
-    }
-
 }
