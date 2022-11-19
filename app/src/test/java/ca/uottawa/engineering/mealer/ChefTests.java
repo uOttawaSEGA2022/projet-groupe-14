bdc7acd3e9d21c73cd538cda86585b9ad4f1d2ce
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-11-19 16:57:43 -0500

    unit test empty templates

---
+++ app/src/test/java/ca/uottawa/engineering/mealer/ChefTests.java
@@ -0,0 +1,14 @@
+package ca.uottawa.engineering.mealer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ChefTests {
+
+    @Test
+    public void isSuspendedTest() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
---
+++ app/src/test/java/ca/uottawa/engineering/mealer/MealTests.java
@@ -0,0 +1,14 @@
+package ca.uottawa.engineering.mealer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MealTests {
+
+    @Test
+    public void mealEqualsTest() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
