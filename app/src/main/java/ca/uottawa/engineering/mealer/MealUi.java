7a14837b3b27f77b00a2afb90a82968cba01c770
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-11-18 23:57:00 -0500

    can delete meals - todo: add notification

--- app/src/main/java/ca/uottawa/engineering/mealer/MealUi.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/MealUi.java
@@ -1,13 +1,28 @@
 package ca.uottawa.engineering.mealer;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import ca.uottawa.engineering.mealer.classes.Meal;
 
 public class MealUi extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    private final String TAG = "meal-UI";
 
     Meal meal;
 
@@ -22,4 +37,28 @@ public class MealUi extends AppCompatActivity {
         TextView mealName = (TextView) findViewById(R.id.nomDuMeal);
         mealName.setText(meal.getName());
     }
+
+    public void deleteMeal(View view) {
+        db.collection("users/" + mAuth.getUid() + "/menu/")
+                .whereEqualTo("name", meal.getName())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                document.getReference().delete();
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void addToProposedMeals(View view) {
+
+    }
+
 }
