da115e798eaf9601e8c4696f8c80a63fddf654b0
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-11-19 16:35:46 -0500

    disable chef buttons if chef is suspended

--- app/src/main/java/ca/uottawa/engineering/mealer/ChefPage.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/ChefPage.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -74,7 +75,13 @@ public class ChefPage extends AppCompatActivity {
         } else {
             long days = (date.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000);
             suspension = String.format("suspended for %s days", days);
-            // send to suspension page
+            Button menuButton = (Button) findViewById(R.id.myMenuButton);
+            Button pMenuButton = (Button) findViewById(R.id.myProposedMealsButton);
+            Button addMealButon = (Button) findViewById(R.id.addMealButton);
+
+            menuButton.setEnabled(false);
+            pMenuButton.setEnabled(false);
+            addMealButon.setEnabled(false);
         }
 
 
