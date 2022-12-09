1666a00c7b882030f8209977da43931f2a8da288
Yousssefch <youssefchakir003@gmail.com>
2022-12-09 01:09:35 -0500

    implemented lists

--- app/src/main/java/ca/uottawa/engineering/mealer/Clientpage.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/Clientpage.java
@@ -1,21 +1,77 @@
 package ca.uottawa.engineering.mealer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
 import ca.uottawa.engineering.mealer.adapter.CustomBaseAdapter;
+import ca.uottawa.engineering.mealer.classes.Meal;
 
 public class Clientpage extends AppCompatActivity {
     ListView listview;
+    private final String TAG = "Clientpage";
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private ArrayList<Meal> meals = new ArrayList<>();
+    private CustomBaseAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clientpage);
+        retrieveProposedMenu();
+        Log.d(TAG,String.valueOf(meals.size()));
         listview = (ListView)findViewById(R.id.mealList);
-        CustomBaseAdapter adapter = new CustomBaseAdapter(getApplicationContext());
+        adapter = new CustomBaseAdapter(getApplicationContext(),meals);
         listview.setAdapter(adapter);
+    }
+    public void retrieveProposedMenu() {
+
+        // restrict to current chef's meals?
+        db.collection("propMeals")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+
+                                // get meal from reference
+                                DocumentReference mealRef = (DocumentReference) document.get("mealRef");
+                                mealRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                        Log.d(TAG, documentSnapshot.getId() + " => " + documentSnapshot.getData());
+                                        Meal meal = documentSnapshot.toObject(Meal.class);
+                                         meals.add(meal);
+                                        adapter.notifyDataSetChanged();
+                                    }
+                                });
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
     }
 }
--- app/src/main/java/ca/uottawa/engineering/mealer/adapter/CustomBaseAdapter.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/adapter/CustomBaseAdapter.java
@@ -5,19 +5,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import ca.uottawa.engineering.mealer.R;
+import ca.uottawa.engineering.mealer.classes.Meal;
 
 public class CustomBaseAdapter extends BaseAdapter {
     Context context;
     LayoutInflater inflater;
-    public CustomBaseAdapter(Context ctx){
+    ArrayList<Meal> listmeal;
+    public CustomBaseAdapter(Context ctx, ArrayList<Meal> mealList){
+        this.listmeal = mealList;
         this.context = ctx;
         inflater = LayoutInflater.from(ctx);
     }
     @Override
     public int getCount() {
-        return 5;
+        return listmeal.size();
     }
 
     @Override
@@ -33,6 +39,8 @@ public class CustomBaseAdapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         view = inflater.inflate(R.layout.activity_meal_list_ui,null);
+        TextView mealname = (TextView) view.findViewById(R.id.mealName);
+        mealname.setText(listmeal.get(i).getName());
         return view;
     }
 }
