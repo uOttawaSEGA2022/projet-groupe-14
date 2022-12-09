8fe800c4cbad228053cd55cc68a891d08715558c
Yousssefch <youssefchakir003@gmail.com>
2022-12-09 15:26:32 -0500

    implemented searchbar

--- app/src/main/java/ca/uottawa/engineering/mealer/Clientpage.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/Clientpage.java
@@ -3,12 +3,15 @@ package ca.uottawa.engineering.mealer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -35,10 +38,13 @@ public class Clientpage extends AppCompatActivity {
     private ArrayList<Meal> meals = new ArrayList<>();
     private CustomBaseAdapter adapter;
 
+    SearchView searchbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clientpage);
+        searchbar = findViewById(R.id.search_bar);
         retrieveProposedMenu();
         Log.d(TAG,String.valueOf(meals.size()));
         listview = (ListView)findViewById(R.id.mealList);
@@ -49,6 +55,27 @@ public class Clientpage extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Meal meal= (Meal) adapterView.getItemAtPosition(i);
                 Log.d(TAG,"clicked");
+            }
+        });
+
+        searchbar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if(TextUtils.isEmpty(s)){
+                    Log.d(TAG, "empty");
+                    adapter.filter("");
+                    listview.clearTextFilter();
+                }
+                else {
+                    adapter.filter(s);
+
+                }
+                return true;
             }
         });
 
--- app/src/main/java/ca/uottawa/engineering/mealer/adapter/CustomBaseAdapter.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/adapter/CustomBaseAdapter.java
@@ -1,6 +1,7 @@
 package ca.uottawa.engineering.mealer.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import ca.uottawa.engineering.mealer.R;
 import ca.uottawa.engineering.mealer.classes.Meal;
@@ -16,11 +18,17 @@ public class CustomBaseAdapter extends BaseAdapter {
     Context context;
     LayoutInflater inflater;
     ArrayList<Meal> listmeal;
-    public CustomBaseAdapter(Context ctx, ArrayList<Meal> mealList){
+    ArrayList<Meal> searchlist;
+    boolean isdoable=true;
+
+    public CustomBaseAdapter(Context ctx, ArrayList<Meal> mealList) {
         this.listmeal = mealList;
+        this.searchlist = new ArrayList<Meal>();
         this.context = ctx;
         inflater = LayoutInflater.from(ctx);
+        Log.d("Clientpage", String.valueOf(searchlist.size()));
     }
+
     @Override
     public int getCount() {
         return listmeal.size();
@@ -38,9 +46,30 @@ public class CustomBaseAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        view = inflater.inflate(R.layout.activity_meal_list_ui,null);
+        view = inflater.inflate(R.layout.activity_meal_list_ui, null);
         TextView mealname = (TextView) view.findViewById(R.id.mealName);
         mealname.setText(listmeal.get(i).getName());
         return view;
+    }
+
+    public void filter(String text) {
+        if (isdoable==true){
+            searchlist.addAll(listmeal);
+            isdoable = false;
+        }
+        listmeal.clear();
+        if (text.equals("")) {
+            listmeal.addAll(searchlist);
+        }
+        else {
+            for (Meal meal : searchlist) {
+                if (meal.getName().equals(text)) {
+                    listmeal.clear();
+                    listmeal.add(meal);
+                }
+            }
+
+        }
+        notifyDataSetChanged();
     }
 }
