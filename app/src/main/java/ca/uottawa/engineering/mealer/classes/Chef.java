e8131fad335ff25714c57b3e61b20bae426a5e12
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-12-09 21:37:12 -0500

    Modified Chef class to hold review values

--- app/src/main/java/ca/uottawa/engineering/mealer/classes/Chef.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/classes/Chef.java
@@ -10,13 +10,18 @@ public class Chef extends User {
     private String cheque;
     private String description;
 
+    private int reviewTotal;
+    private int reviewCount;
+
     private Date suspension;
 
     /**
      * Empty constructor used for Firebase. (DON'T USE, USE FULL CONSTRUCTOR!).
      */
     public Chef() {
-    };
+    }
+
+    ;
 
     /**
      * Constructor for Chef
@@ -33,8 +38,35 @@ public class Chef extends User {
         this.cheque = cheque;
         this.description = desc;
         this.suspension = new Date(0);
+        this.reviewTotal = 0;
+        this.reviewCount = 0;
+    }
 
-        // Get meal list from firebase
+    /**
+     * Constructor for Chef
+     *
+     * @param nickname    Nickname of chef
+     * @param name        Name of chef
+     * @param email       Email address of chef
+     * @param address     Pick-up address of chef
+     * @param cheque      Cheque information
+     * @param desc        Description of chef
+     * @param reviewTotal Total review value
+     * @param reviewCount Total reviews
+     */
+    public Chef(String nickname, String name, String email, String address, String cheque,
+                String desc, int reviewTotal, int reviewCount) {
+        super(nickname, name, email, address, "chef"); // hardcoded chef role
+        this.cheque = cheque;
+        this.description = desc;
+        this.suspension = new Date(0);
+        this.reviewTotal = reviewTotal;
+        this.reviewCount = reviewCount;
+    }
+
+    public void addReview(int val) {
+        reviewTotal += val;
+        reviewCount++;
     }
 
     // Check if banned
