dced0edded561c90bddde6b80ae97809fe97db60
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-12-09 22:08:19 -0500

    Removed mAuth from ChefHandler

--- app/src/main/java/ca/uottawa/engineering/mealer/handlers/ChefHandler.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/handlers/ChefHandler.java
@@ -6,7 +6,6 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -16,7 +15,6 @@ import ca.uottawa.engineering.mealer.classes.Chef;
 
 public class ChefHandler {
 
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final String TAG = "Chef-Handler";
 
