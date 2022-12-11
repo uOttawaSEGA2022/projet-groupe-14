457348f03bdb6c3f62360d956d08450fe1fda90b
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-12-10 20:59:51 -0500

    Show Meal info at clientMeal page

--- app/src/main/java/ca/uottawa/engineering/mealer/ClientMeal.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/ClientMeal.java
@@ -11,14 +11,16 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
-
+import ca.uottawa.engineering.mealer.classes.Chef;
 import ca.uottawa.engineering.mealer.classes.Meal;
 import ca.uottawa.engineering.mealer.classes.Order;
 import ca.uottawa.engineering.mealer.helpers.ClientHandler;
@@ -31,11 +33,12 @@ public class ClientMeal extends AppCompatActivity {
 
     private final String TAG = "Clientmeal";
     private MealHandler mealHandler;
+    private Chef chef;
     private Meal meal;
     private String username;
-    DocumentReference mealRef;
-    private ArrayList<Meal> orderedmeals = new ArrayList<>();
     private final ClientHandler clientHandler = ClientSingleton.getInstance();
+
+    TextView chefReviews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +50,23 @@ public class ClientMeal extends AppCompatActivity {
         mealHandler = new MealHandler(meal.getName());
         username = clientHandler.getClient().getName();
 
-        TextView chefname = (TextView) findViewById(R.id.chefName);
+        getChefInfo();
 
-        chefname.setText(meal.getChefName());
+        TextView chefName = (TextView) findViewById(R.id.chefName);
+        chefName.setText(meal.getChefName());
+        chefReviews = (TextView) findViewById(R.id.chefReviews);
+        TextView clientMealName = (TextView) findViewById(R.id.clientMealName);
+        clientMealName.setText(meal.getName());
+        TextView mealDescription = (TextView) findViewById(R.id.mealDescription);
+        mealDescription.setText(meal.getDesc());
+        TextView mealIngredients = (TextView) findViewById(R.id.mealIngredients);
+        mealIngredients.setText(meal.getIngredients());
+        TextView mealType = (TextView) findViewById(R.id.mealType);
+        mealType.setText(meal.getCuisineType());
+        TextView mealAllergies = (TextView) findViewById(R.id.mealAllergies);
+        mealAllergies.setText(meal.getAllergies());
+        TextView mealPrice = (TextView) findViewById(R.id.mealPrice);
+        mealPrice.setText(meal.getCost());
     }
 
     public void addToOrderedMeals(View view) throws InterruptedException {
@@ -77,9 +94,41 @@ public class ClientMeal extends AppCompatActivity {
                 });
         onBackPressed();
     }
-    public  void onClickProfile(View view){
+
+    public void getChefInfo() {
+        db.collection("users")
+                .whereEqualTo("name", meal.getChefName())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot document = task.getResult();
+                            Log.d(TAG, document.getQuery().toString());
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                Log.d(TAG, documentSnapshot.getId() + " => " + documentSnapshot.getData());
+                                chef = documentSnapshot.toObject(Chef.class);
+
+                                String rating;
+                                double ratingNumber = chef.getReview();
+                                if (ratingNumber == 0) {
+                                    rating = "No Reviews";
+                                } else {
+                                    rating = String.format("%.1f out of 5 stars", ratingNumber);
+                                }
+
+                                chefReviews.setText(rating);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void onClickProfile(View view) {
         Intent switchActivityIntent = new Intent(this, chefProfileinfo.class);
-        switchActivityIntent.putExtra("chefname",meal.getChefName());
+        switchActivityIntent.putExtra("chefname", meal.getChefName());
         startActivity(switchActivityIntent);
     }
-    }
+}
