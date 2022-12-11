35ddd72cad67ba645d532c7989842aa7cc488a76
Syed Yead Zaman <s.yead.zaman@gmail.com>
2022-12-10 20:38:49 -0500

    Get chef info and set reviews text

--- app/src/main/java/ca/uottawa/engineering/mealer/chefProfileinfo.java
+++ app/src/main/java/ca/uottawa/engineering/mealer/chefProfileinfo.java
@@ -1,47 +1,46 @@
 package ca.uottawa.engineering.mealer;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import ca.uottawa.engineering.mealer.classes.Chef;
-import ca.uottawa.engineering.mealer.classes.Meal;
-import ca.uottawa.engineering.mealer.classes.Order;
-import ca.uottawa.engineering.mealer.helpers.ChefHandler;
-import ca.uottawa.engineering.mealer.helpers.ChefSingleton;
+import ca.uottawa.engineering.mealer.helpers.ComplaintHandler;
 
 public class chefProfileinfo extends AppCompatActivity {
-    TextView rating;
+    TextView rateText;
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private String TAG = "chef_profile";
-    private String chefname;
-    private Chef cheff;
+    private String chefName;
+    private Chef chef;
+    private DocumentReference chefRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_profileinfo);
-        Bundle extras = getIntent().getExtras();
-        chefname = (String) extras.get("chefname");
-        getchef();
 
-        rating = findViewById(R.id.Rate);
+        Bundle extras = getIntent().getExtras();
+        chefName = (String) extras.get("chefname");
+        getChefInfo();
+
+        rateText = findViewById(R.id.RateText);
     }
     public  void onRate(View view){
         Intent switchActivityIntent = new Intent(this, ClientRatingsActivity.class);
@@ -49,7 +48,7 @@ public class chefProfileinfo extends AppCompatActivity {
     }
     public void getchef(){
         db.collection("users")
-                .whereEqualTo("name",chefname)
+                .whereEqualTo("name", chefName)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -57,8 +56,20 @@ public class chefProfileinfo extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             QuerySnapshot document = task.getResult();
                             Log.d(TAG, document.getQuery().toString());
-                            for (QueryDocumentSnapshot documentt : task.getResult()){
-                                Log.d(TAG, documentt.getId() + " => " + documentt.getData());
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                Log.d(TAG, documentSnapshot.getId() + " => " + documentSnapshot.getData());
+                                chef = documentSnapshot.toObject(Chef.class);
+                                chefRef = documentSnapshot.getReference();
+
+                                String rating;
+                                double ratingNumber = chef.getReview();
+                                if (ratingNumber == 0) {
+                                    rating = "No Reviews";
+                                } else {
+                                    rating = String.format("%.1f out of 5 stars", ratingNumber);
+                                }
+
+                                rateText.setText(rating);
                             }
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
